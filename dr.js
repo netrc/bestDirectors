bebd184556c424a3c2b816f380ada28862f4bffc
richard <ric@netrc.com>
2014-01-25 09:01:25 -0500

    small edits to dogetBest, background color

--- dr.js
+++ dr.js
@@ -1,9 +1,10 @@
 var request = require('request');
 
 var drdata = [];
+var ms1,ms2,ms3,ms4;
 
 var dogetBest = function(minNumVotes, minNumMovies) {
-    var d1 = new Date();
+    ms1 = Date.now();
     var r = {};
     // collect all the movies and sum all the ratings for each director
     drdata.forEach(function(x){
@@ -13,7 +14,7 @@ var dogetBest = function(minNumVotes, minNumMovies) {
         r[n].totalR += x.r;
         r[n].numR++;
     });
-    var d2 = new Date();
+    ms2 = Date.now();
     // finally, do the averages and sort
     var ans = [];
     var totalN =0; var totalR=0;
@@ -25,23 +26,24 @@ var dogetBest = function(minNumVotes, minNumMovies) {
         r[k].durl = "http://imdb.com/find?s=nm&q="+k;
         ans.push(r[k]);
     }
+    ms3 = Date.now();
     ans.sort(function(a,b){ return (b.avgR - a.avgR)});
-    var d3 = new Date();
-    console.log("getBest getVotes: " + (d2.getTime()-d1.getTime()) + " getFilms&sort: "+(d3.getTime()-d2.getTime()));
-    return { ans:ans, totalN:totalN, totalAvg: (totalR/totalN).toFixed(2), totalD: ans.length, queryMS:(d3.getTime()-d2.getTime()) };
+    ms4 = Date.now();
+    console.log("dogetbest total: " + (ms4-ms1) + "  votes: " + (ms2-ms1) + " avgr: "+(ms3-ms2) +" sort: "+(ms4-ms3));
+    return { ans:ans, totalN:totalN, totalAvg: (totalR/totalN).toFixed(2), totalD: ans.length, queryMS:(ms4-ms1) };
 };
 
 exports.getBest = function(minNumVotes, minNumMovies) {
     if (drdata.length === 0) {
         console.log("getting drdata");
         request('http://netrc.com/drFoundSorted.json', function (error, response, body) {
-            console.log("got drdata status: "+response.statusCode);
-            console.log(" body.length: "+response.body.length);
-            console.log(" body(0-100): "+response.body.substring(0,100));
-            console.log(" body(-100-end): "+response.body.substring(response.body.length-100));
+            //console.log("got drdata status: "+response.statusCode);
+            //console.log(" body.length: "+response.body.length);
+            //console.log(" body(0-100): "+response.body.substring(0,100));
+            //console.log(" body(-100-end): "+response.body.substring(response.body.length-100));
             if (!error && response.statusCode == 200) {
                 drdata = JSON.parse(response.body);
-                console.log("drdata parsed length: " + drdata.length);
+                console.log("drdata parsed length: " + drdata.length + " .. " + (drdata.length==206194)?"ok":"WRONG!");
                 return dogetBest(minNumVotes, minNumMovies);
             } else {
                 console.log("bad status:" + response.statusCode + "error: "+error);
